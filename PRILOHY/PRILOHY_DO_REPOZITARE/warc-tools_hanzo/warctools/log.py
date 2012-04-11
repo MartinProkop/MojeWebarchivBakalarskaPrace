de895167fadbb70427e6e92190d9a60b9d9d1027
Martin Prokop <xixaom@centrum.cz>
2012-04-11 11:58:44 +0200

    Pridany prilohy - vsechny tri nastroje na warc - bez mych zasahu

--- PRILOHY/PRILOHY_DO_REPOZITARE/warc-tools_hanzo/warctools/log.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-__all__ = ['debug']
-
-debug = lambda:None
-
-if __debug__:
-    def debug(*args):
-        print >> sys.stderr, 'WARCTOOLS',args
--- PRILOHY/PRILOHY_DO_REPOZITARE/warc-tools_kpk09/warctools/__init__.py
+++
@@ -1,9 +0,0 @@
-from warctools.record import ArchiveRecord
-from warctools.warc import WarcRecord
-from warctools.arc import ArcRecord
-
-__all__= [
-    'ArchiveRecord',
-    'ArcRecord',
-    'WarcRecord',
-]
