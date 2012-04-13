5e89ba801d787fefd6c05a1919ef5dad788750b2
Martin Prokop <xixaom@centrum.cz>
2012-04-13 18:31:07 +0200

    Uprava textu - povidani o JHOVE2 a jeho uprave Pridana priloha JHOVE2 - pridana  trida na zpousteni z ARC2WARC

---
+++ PRILOHY/RunFromARC2WARC.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2012 Martin Prokop.
+ */
+package org.jhove2.app;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Array;
+import org.jhove2.core.JHOVE2Exception;
+
+/**
+ * Vlastní třída přidaná k progragramu JHOVE2. Slouží ke spouštění
+ * JHOVE2 z nástroje pro migraci arc souborů (warctools: arc2warc).
+ * Nástroj arc2warc je implementován v Pythonu. Pomocí nástroje Jpype
+ * (slouží ke spuštění JVM nad programem v Pythonu) mohu spouštět tuto třídu.
+ * 
+ * @author Martin Prokop
+ */
+public class RunFromARC2WARC {
+
+    /**
+     * Metoda dostane na vstup seznam souborů, které předá ke zpracování
+     * aplikaci JHOVE2 a nakonec na vypíše do zadaného souboru výstup z JHOVE2.
+     * @param contents Pole cest k souborům
+     * @param path Cesta k souboru, do kterého se mají vypsat informace
+     * @throws IOException
+     * @throws JHOVE2Exception 
+     */
+    public void runJHOVE(String contents[], String path) throws IOException, JHOVE2Exception {
+
+        // Presmerovani vystupu z JHOVE2 do /dev/null
+        OutputStream output = new FileOutputStream("/dev/null");
+        PrintStream nullOut = new PrintStream(output);
+        System.setErr(nullOut);
+        System.setOut(nullOut);
+
+        String[] data = {"-o", path, "-d", "XML"};
+
+        String[] seznam = arrayMerge(data, contents);
+
+        JHOVE2CommandLine.main(seznam);
+    }
+
+    public static void main(String[] args) throws IOException, JHOVE2Exception {
+    }
+
+    /**
+     * Pomocná metoda, dostane dvě pole Stringů a spojí je.
+     * @param <T> Seznam polí Stringů
+     * @param arrays Pole
+     * @return Spojená pole Stringů
+     */
+    public static <T> T[] arrayMerge(T[]... arrays) {
+        int count = 0;
+        for (T[] array : arrays) {
+            count += array.length;
+        }
+        T[] mergedArray = (T[]) Array.newInstance(arrays[0][0].getClass(), count);
+
+        int start = 0;
+        for (T[] array : arrays) {
+            System.arraycopy(array, 0,
+                    mergedArray, start, array.length);
+            start += array.length;
+        }
+        return (T[]) mergedArray;
+    }
+}
